a6d8b8312e7f66ecfa12e752c3667e7f51e303ea
kumar sundram <104027247+ksundram620@users.noreply.github.com>
2023-12-05 10:52:35 +0530

    Footer

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Header from './components/Header/Header';
 import Crousal from './components/crousal/Crousal';
+import Footer from './components/Fotter/Footer'
 
 function App() {
   return (
     <>
       <Header/>
       <Crousal/>
+      <Footer/>
     </>
   );
 }
